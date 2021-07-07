87352f7fcb8ef183bb6e0fa7add6c7fddbf73082
Fasica <fasicasr@gmail.com>
2021-07-07 14:54:09 -0400

    configure webpack. Include babel and bundel.js

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require('path');
+
+console.log("HII")
+
+const config = {
+  entry: {
+    app: './public/assets/js/index.js',
+    // favorites: './assets/js/favorites.js',
+    // topic: './assets/js/topic.js',
+  },
+  output: {
+    path: __dirname + 'public/dist',
+    filename: 'bundle.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      name: 'Budget Tracker',
+      short_name: 'Budget Tracker',
+      description: 'A budget tracking app that runs offline',
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      // 'theme-color': '#36465d;',
+      start_url: '/',
+      icons: [
+        {
+          src: path.resolve('public/assets/icons/icon-512x512.png'),
+          sizes: [72, 96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+  ],
+};
+
+console.log(config)
+
+module.exports = config;
